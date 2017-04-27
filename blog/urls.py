e052a6237dadc47c5cc4a19b8f193845be516050
Zachary <zacharythunt@gmail.com>
2017-04-26 17:22:47 -0700

    Changed html for the site.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# import django's url function
+from django.conf.urls import url
+# import all of our views from the blog application
+from . import views
+
+urlpatterns = [
+  url(r'^$', views.post_list, name='post_list'),
+]
